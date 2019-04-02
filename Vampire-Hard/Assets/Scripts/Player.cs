94f49323a9740238434040fee70b6e31765e547b
osinmv <osinmv@gmail.com>
2019-04-01 21:28:56 -0400

    animation
    аниме для ыыыыыыыыы

--- Vampire-Hard/Assets/Scripts/Player.cs
+++ Vampire-Hard/Assets/Scripts/Player.cs
@@ -6,6 +6,10 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float speed = 500f;
+    public bool started_sucking;
+    public Animator animator;
+    private float timer;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +19,26 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            animator.SetBool("Walking", false);
+            started_sucking = true;
+        }
+
+        if (started_sucking)
+        {
+            timer += Time.deltaTime;
+            if (timer > 1)
+            {
+                animator.SetBool("Walking", true);
+                started_sucking = false;
+                timer = 0;
+            }
+        }
         
+
     }
+
     // Updated is called fixed amount of times per
     private void FixedUpdate()
     {
