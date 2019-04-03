ced96b566d14dc28f83188a7ceb0a7f27a7e6691
osinmv <osinmv@gmail.com>
2019-04-03 17:23:43 -0400

    yo
    Леха лучше человека, Леха лучший просто так.
    Есть у Лехи чувство веса
    Леха нужен для картин
    Был у Лехи ранец, стал у Лехи кнут(Им гнут)

--- Vampire-Hard/Assets/Scripts/HumansCoordinator.cs
+++ Vampire-Hard/Assets/Scripts/HumansCoordinator.cs
@@ -7,20 +7,42 @@ using System.Linq;
 public class HumansCoordinator : MonoBehaviour
 {
 
-    public GameObject pref_human;
-    public Object[] hats;
-    private Sprite head;
-    public Sprite[] body;
+    public GameObject pref_human_nicy;
+    public GameObject pref_human_brown;
+    public GameObject pref_human_soldier;
+    private Sprite[] hats;
+    private GameObject[] crowd;
 
     void Awake()
     {
         hats = Resources.LoadAll("Sprites/hats", typeof(Sprite)).Cast<Sprite>().ToArray();
-        head = Resources.Load("Sprites/hats", typeof(Sprite))as Sprite;
-        body = Resources.LoadAll("Sprites/bodys", typeof(Sprite)).Cast<Sprite>().ToArray();
     }
+
+
+    void set_Hair(GameObject a)
+    {
+        /// GameObject b = a.transform.FindChild("Hair");
+    }
+    
     void Start()
     {
-        
+        for (int i = 0; i < 10; i++)
+        {
+            int a = (int)Random.Range(0, 2);
+            if (a == 0)
+                {
+                    crowd[i] = Instantiate(pref_human_brown);
+                    set_Hair(crowd[i]);
+                }
+            else if (a == 1) 
+                {
+                    crowd[i] = Instantiate(pref_human_soldier);
+                }
+            else
+                {
+                    crowd[i] = Instantiate(pref_human_nicy);
+                }
+        }
     }
 
     // Update is called once per frame
--- Vampire-Hard/Assets/Scripts/Personality.cs
+++ Vampire-Hard/Assets/Scripts/Personality.cs
@@ -10,20 +10,24 @@ public class Personality : MonoBehaviour
     public bool silver = false;
     public bool set_goal = false;
     public Vector2 current_goal;
-    // Start is called before the first frame update
+    public float time_for_action = 10f;
+    private Rigidbody2D rgb;
+
     void Start()
     {
-        
+        rgb = gameObject.GetComponent<Rigidbody2D>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 vect = new Vector2(current_goal.x - gameObject.transform.position.x, current_goal.y - gameObject.transform.position.y);
+        gameObject.transform.right = new Vector2(current_goal.x - gameObject.transform.position.x, current_goal.y - gameObject.transform.position.y);
+        rgb.velocity = vect;
+
     }
     public void setTarget(Vector2 target)
     {
-
+        current_goal = target;
     }
 
 
