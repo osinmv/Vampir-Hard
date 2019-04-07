297e356d4ed2669a7f7d3e992b7a654fc71faab8
osinmv <osinmv@gmail.com>
2019-04-07 13:37:09 -0400

    ye

--- Vampire-Hard/Assets/Scripts/HumansCoordinator.cs
+++ Vampire-Hard/Assets/Scripts/HumansCoordinator.cs
@@ -12,8 +12,9 @@ public class HumansCoordinator : MonoBehaviour
     public GameObject pref_human_soldier;
     private Sprite[] hats;
     private List<GameObject> crowd = new List<GameObject>();
+    private Personality[] crowd_object;
     private Color[] cols = new Color[] {Color.green,Color.yellow,Color.magenta,Color.red,Color.cyan,Color.blue,Color.white};
-
+    public GameObject[] tables;
     void Awake()
     {
         hats = Resources.LoadAll("Sprites/hats", typeof(Sprite)).Cast<Sprite>().ToArray();
@@ -29,9 +30,10 @@ public class HumansCoordinator : MonoBehaviour
     
     void Start()
     {
+        crowd_object = new Personality[1];
         for (int i = 0; i < 1; i++)
         {
-            int a = (int)Random.Range(0, 3);
+            int a = (int)Random.Range(0, 1);
             if (a == 0)
                 {
                     crowd.Add(Instantiate(pref_human_brown));
@@ -45,13 +47,30 @@ public class HumansCoordinator : MonoBehaviour
                     crowd.Add(Instantiate(pref_human_nicy));
                 }
             set_Hair(crowd[i]);
-            crowd[i].GetComponent<Personality>().setTarget(new Vector2(-10, -10));
+            crowd_object[i] = crowd[i].GetComponent<Personality>();
+            crowd_object[i].setTarget(new Vector2(Random.Range(-6,6), Random.Range(-4, 4)));
+            
+            
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < crowd_object.Length; i++)
+        {
+            if (crowd_object[i].reached_goal)
+            {
+                if (Random.Range(0, 1) > 0.5)
+                {
+                    crowd_object[i].set_random_goal(new Vector2(Random.Range(-6, 6), Random.Range(-4, 4)));
+                }
+                else
+                {
+                    crowd_object[i].setTarget(tables[(int)Random.Range(0,tables.Length)].transform.position);
+                }
+            }
+            
+        }
     }
 }
--- Vampire-Hard/Assets/Scripts/Player.cs
+++ Vampire-Hard/Assets/Scripts/Player.cs
@@ -9,45 +9,41 @@ public class Player : MonoBehaviour
     public bool started_sucking;
     public Animator animator;
     private float timer;
-   
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E)&&!started_sucking)
         {
-            animator.SetBool("Walking", false);
-            started_sucking = true;
+            CancelInvoke();
+            animator.Play("MainCharacterSuck",0);
             rb.velocity = new Vector2(0, 0);
-            
+            Invoke("set_Animation_walk",1);
+            started_sucking = true;
         }
 
-        if (started_sucking)
-        {
-            timer += Time.deltaTime;
-            if (timer > 1)
-            {
-                animator.SetBool("Walking", true);
-                started_sucking = false;
-                timer = 0;
-            }
-        }
         
+
 
     }
 
+    void set_Animation_walk()
+    {
+        animator.Play("Walking");
+        started_sucking = false;
+    }
     // Updated is called fixed amount of times per
     private void FixedUpdate()
     {
         if (!started_sucking)
         {
             Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            
             transform.right = new Vector2(mouse.x - gameObject.transform.position.x, mouse.y - gameObject.transform.position.y);
             rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime, Input.GetAxis("Vertical") * speed * Time.fixedDeltaTime);
         }
